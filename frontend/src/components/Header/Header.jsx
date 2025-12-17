52a0888474434a3b6204421403918e77732cbbfd
Rohit Bhosale <rohitbhosale409@gamil.com>
2025-12-17 23:39:58 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favouite food here</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque mollitia ipsam aliquam provident incidunt. Ratione doloribus non tempora dolor suscipit accusantium, quos rem odit maxime corporis! Harum, nulla hic! Excepturi?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header

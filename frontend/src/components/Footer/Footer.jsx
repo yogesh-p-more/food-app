d7a0ee9fb8d882471407717b6c1160af1912f4f7
Yogesh More <yogesh.more@email.com>
2025-12-18 20:59:59 +0530

    Home page ready

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import {assets} from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus doloribus distinctio fugiat beatae ipsa cumque? Commodi temporibus placeat alias ullam quidem quis. Modi quis reiciendis nulla, a est tenetur architecto!</p>
+                <div className="footer-social-icon">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy </li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+91 8108466415</li>
+                    <li>yogesh03265@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className='footer-copyright'>Copyright 2025 Tomato.com - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
